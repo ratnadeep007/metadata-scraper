2f4e2c3a591b17d8b723c2d79c2d26ed46296d2a
Ratnadeep Bhattacharyya <ratnadeepofghato@gmail.com>
2019-07-10 10:55:28 +0530

    added condition if no image in metadata

--- server/src/shared/service/scrape/scrape.service.ts
+++ server/src/shared/service/scrape/scrape.service.ts
@@ -6,6 +6,10 @@ export class ScrapeService {
 
   async scrapeUrl(url: string) {
     let scrapeData = await scrapeAndExtract(url)
+    if (!scrapeData['image'] || scrapeData['image'] === {}) {
+      scrapeData['image']['type'] = 'image'
+      scrapeData['image']['url'] = 'https://assets.justinmind.com/blog/wp-content/uploads/2018/08/pen-tool-illustration-768x245.png'
+    }
     console.log(scrapeData);
     return scrapeData;
   }
