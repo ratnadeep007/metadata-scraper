1433af90ad6fd25ab2011461187999f7cc34f408
Ratnadeep Bhattacharyya <ratnadeepofghato@gmail.com>
2019-07-10 15:22:44 +0530

    added colors according to emcode and fixed styling issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,57 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
 import Scraping from './components/Scraping';
+import Container from '@material-ui/core/Container';
+import { makeStyles, createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import './App.css';
 
+/**
+ * Few styles
+ */
+const styles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+    padding: 12,
+    color: 'white',
+    fontWeight: 900
+  }
+}));
+
+/**
+ * Theming
+ */
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#52b744'
+    },
+    text: {
+      primary: '#000000',
+    }
+  },
+});
+
+
 const App: React.FC = () => {
+  const classes = styles();
   return (
-    <div>
-      <Scraping/>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Typography variant="h5" className={classes.title}>
+            Emcode Scraper
+          </Typography>
+        </AppBar>
+        <Container>
+          <Scraping/>
+        </Container>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- client/src/components/Scraping.tsx
+++ client/src/components/Scraping.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
 
 class Scraping extends React.Component {
+
   render() {
     return (
-      <div>
-        <h1>Hello</h1>
+      <div style={{ flexGrow: 1, marginTop: 15 }}>
+        <Grid
+          container
+          direction="row"
+          spacing={3}
+        >
+          <Grid item xs={10}>
+            <TextField
+              label="URL"
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={2}>
+            <Button 
+              style={{ padding: 15, color: 'white' }} 
+              variant="contained" 
+              color="primary"
+            >
+              Post
+            </Button>
+          </Grid>
+        </Grid>
       </div>
     );
   }
